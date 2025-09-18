55124dcde91f7d99dce4cf121c60de037f2cb335
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:04:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Array1TipoStructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            double[] vect = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += vect[i];
+            }
+            double avg = sum / N;
+            Console.WriteLine("AVERAGE HEIGHT: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
